cc2e1b3377de6684916f77e6e2f55b5178a27544
eunjinnnn <sally0680@naver.com>
2024-04-17 15:56:27 +0900

    first commit

---
+++ forntend/postcss.config.cjs
@@ -0,0 +1,21 @@
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+
+const config = {
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+  ],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  darkMode: 'class',
+};
+
+module.exports = config;
+
